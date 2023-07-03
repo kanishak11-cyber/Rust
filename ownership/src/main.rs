68235246f21fa0f24038fd965e572f37522b6654
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2023-07-04 00:35:27 +0530

    hash map

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,11 +1,20 @@
-fn main(){
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-    println!("The length of '{}' is {}.", s1, len);
+// fn main(){
+//     let s1 = String::from("hello");
+//     let len = calculate_length(&s1);
+//     println!("The length of '{}' is {}.", s1, len);
 
     
-}
+// }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
+use std::collections::HashMap;
+
+fn main(){
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 }
